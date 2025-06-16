3c02120c87f266916ab29423de5e13c97a2ec38a
Sebastian Bejm <125sbejm@gmail.com>
2025-06-16 10:49:13 -0700

    add project filtering

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ function toggleMenu() {
     const navLinks = document.getElementById('nav-links');
     navLinks.classList.toggle('show');
 }
+
+function filterProjects(category) {
+    const projects = document.querySelectorAll('.project');
+    projects.forEach(project => {
+      if (category === 'all' || project.classList.contains(category)) {
+        project.style.display = 'block';
+      } else {
+        project.style.display = 'none';
+      }
+    });
+  }
+  
